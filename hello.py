60d3875487a3671adf6bfac7b5604079b6cded7b
Kapilandra G <kapilandra777@gmail.com>
2024-12-28 23:24:22 +0530

    Adding from Windows

--- hello.py
+++ hello.py
@@ -5,3 +5,8 @@ print(os.getcwd())
 print("Hello Galaxy")
 
 print("This is testing the Git Push with the VS Code Terminal")
+ 
+for i in range(10):
+    print(i)
+
+print("This is the end of the program")
